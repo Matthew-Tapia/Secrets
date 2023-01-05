0f5209e94614a0e01b1110f73355cc4f759dfd99
Matthew Tapia <matthewtapia8@gmail.com>
2023-01-04 21:02:03 -0800

    Added hashing for security

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5")
 
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/userDB');
@@ -19,10 +19,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { 
-    secret: process.env.SECRET,
-    encryptedFields: ["password"]
-});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -36,7 +32,7 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, (err, foundUser) => {
         if(err){
@@ -58,7 +54,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save((err) => {
